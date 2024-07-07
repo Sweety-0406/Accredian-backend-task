a8a0a2b84b314300df5590b81259a037b0af7662
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-07-07 11:53:02 +0530

    another

--- src/server.js
+++ src/server.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self'");
+  next();
+});
+
 // Routes
 app.use('/api', referralRoutes);
 
